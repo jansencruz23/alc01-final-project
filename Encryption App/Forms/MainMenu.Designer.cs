198dba9c990b0495dfe92bbbfb9f532bba258bb0
Jansen Cruz <jansencruz12345@gmail.com>
2024-05-31 16:55:57 +0800

    Design menu

--- Encryption App/Forms/MainMenu.Designer.cs
+++ Encryption App/Forms/MainMenu.Designer.cs
@@ -28,12 +28,150 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "MainMenu";
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges11 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges12 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges9 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges10 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges8 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            guna2Panel3 = new Guna.UI2.WinForms.Guna2Panel();
+            guna2Panel2 = new Guna.UI2.WinForms.Guna2Panel();
+            btnEncryption = new Guna.UI2.WinForms.Guna2Button();
+            guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            guna2Button2 = new Guna.UI2.WinForms.Guna2Button();
+            guna2Panel1.SuspendLayout();
+            guna2Panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // guna2Panel1
+            // 
+            guna2Panel1.BackColor = Color.Transparent;
+            guna2Panel1.BorderRadius = 25;
+            guna2Panel1.Controls.Add(guna2Panel3);
+            guna2Panel1.Controls.Add(guna2Panel2);
+            guna2Panel1.CustomizableEdges = customizableEdges11;
+            guna2Panel1.FillColor = Color.FromArgb(74, 58, 146);
+            guna2Panel1.Location = new Point(169, 36);
+            guna2Panel1.Name = "guna2Panel1";
+            guna2Panel1.ShadowDecoration.BorderRadius = 25;
+            guna2Panel1.ShadowDecoration.CustomizableEdges = customizableEdges12;
+            guna2Panel1.ShadowDecoration.Depth = 10;
+            guna2Panel1.ShadowDecoration.Enabled = true;
+            guna2Panel1.ShadowDecoration.Shadow = new Padding(3, 3, 6, 6);
+            guna2Panel1.Size = new Size(459, 306);
+            guna2Panel1.TabIndex = 0;
+            // 
+            // guna2Panel3
+            // 
+            guna2Panel3.CustomizableEdges = customizableEdges1;
+            guna2Panel3.Location = new Point(-33, 11);
+            guna2Panel3.Name = "guna2Panel3";
+            guna2Panel3.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            guna2Panel3.Size = new Size(506, 64);
+            guna2Panel3.TabIndex = 0;
+            // 
+            // guna2Panel2
+            // 
+            guna2Panel2.BorderRadius = 25;
+            guna2Panel2.Controls.Add(guna2Button2);
+            guna2Panel2.Controls.Add(guna2Button1);
+            guna2Panel2.Controls.Add(btnEncryption);
+            guna2Panel2.CustomizableEdges = customizableEdges9;
+            guna2Panel2.FillColor = Color.FromArgb(111, 88, 202);
+            guna2Panel2.Location = new Point(3, 53);
+            guna2Panel2.Name = "guna2Panel2";
+            guna2Panel2.ShadowDecoration.CustomizableEdges = customizableEdges10;
+            guna2Panel2.Size = new Size(453, 250);
+            guna2Panel2.TabIndex = 0;
+            // 
+            // btnEncryption
+            // 
+            btnEncryption.BackColor = Color.FromArgb(111, 88, 202);
+            btnEncryption.BorderColor = Color.Silver;
+            btnEncryption.BorderRadius = 7;
+            btnEncryption.CustomizableEdges = customizableEdges7;
+            btnEncryption.DisabledState.BorderColor = Color.DarkGray;
+            btnEncryption.DisabledState.CustomBorderColor = Color.DarkGray;
+            btnEncryption.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btnEncryption.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btnEncryption.FillColor = Color.White;
+            btnEncryption.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            btnEncryption.ForeColor = Color.Black;
+            btnEncryption.Location = new Point(22, 41);
+            btnEncryption.Name = "btnEncryption";
+            btnEncryption.ShadowDecoration.CustomizableEdges = customizableEdges8;
+            btnEncryption.Size = new Size(406, 57);
+            btnEncryption.TabIndex = 1;
+            btnEncryption.Text = "guna2Button1";
+            // 
+            // guna2Button1
+            // 
+            guna2Button1.BackColor = Color.FromArgb(111, 88, 202);
+            guna2Button1.BorderColor = Color.Silver;
+            guna2Button1.BorderRadius = 7;
+            guna2Button1.CustomizableEdges = customizableEdges5;
+            guna2Button1.DisabledState.BorderColor = Color.DarkGray;
+            guna2Button1.DisabledState.CustomBorderColor = Color.DarkGray;
+            guna2Button1.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            guna2Button1.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            guna2Button1.FillColor = Color.White;
+            guna2Button1.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            guna2Button1.ForeColor = Color.Black;
+            guna2Button1.Location = new Point(22, 107);
+            guna2Button1.Name = "guna2Button1";
+            guna2Button1.ShadowDecoration.CustomizableEdges = customizableEdges6;
+            guna2Button1.Size = new Size(406, 57);
+            guna2Button1.TabIndex = 2;
+            guna2Button1.Text = "guna2Button1";
+            // 
+            // guna2Button2
+            // 
+            guna2Button2.BackColor = Color.FromArgb(111, 88, 202);
+            guna2Button2.BorderColor = Color.Silver;
+            guna2Button2.BorderRadius = 7;
+            guna2Button2.CustomizableEdges = customizableEdges3;
+            guna2Button2.DisabledState.BorderColor = Color.DarkGray;
+            guna2Button2.DisabledState.CustomBorderColor = Color.DarkGray;
+            guna2Button2.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            guna2Button2.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            guna2Button2.FillColor = Color.White;
+            guna2Button2.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            guna2Button2.ForeColor = Color.Black;
+            guna2Button2.Location = new Point(22, 173);
+            guna2Button2.Name = "guna2Button2";
+            guna2Button2.ShadowDecoration.CustomizableEdges = customizableEdges4;
+            guna2Button2.Size = new Size(406, 57);
+            guna2Button2.TabIndex = 3;
+            guna2Button2.Text = "guna2Button2";
+            // 
+            // MainMenu
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(118, 95, 209);
+            ClientSize = new Size(800, 378);
+            Controls.Add(guna2Panel1);
+            Name = "MainMenu";
+            Text = "MainMenu";
+            guna2Panel1.ResumeLayout(false);
+            guna2Panel2.ResumeLayout(false);
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel2;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel3;
+        private Guna.UI2.WinForms.Guna2Button btnEncryption;
+        private Guna.UI2.WinForms.Guna2Button guna2Button2;
+        private Guna.UI2.WinForms.Guna2Button guna2Button1;
     }
 }
