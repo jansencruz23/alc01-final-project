5a721f3920dbff0ce212ba199ad55d06634867e6
Aljunalei Alfonso <alfonsoaljunaleim4@gmail.com>
2024-05-31 21:32:35 +0800

    to pull

--- Encryption App/Forms/XORForm.Designer.cs
+++ Encryption App/Forms/XORForm.Designer.cs
@@ -28,12 +28,150 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "XORForm";
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges9 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges10 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges11 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges12 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges13 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges14 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges15 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges16 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            button_Encrypt = new Guna.UI2.WinForms.Guna2Button();
+            guna2HtmlLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            tbWord = new Guna.UI2.WinForms.Guna2TextBox();
+            guna2HtmlLabel2 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            tbKey = new Guna.UI2.WinForms.Guna2TextBox();
+            labelEncrypted = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            button_Decrypt = new Guna.UI2.WinForms.Guna2Button();
+            SuspendLayout();
+            // 
+            // button_Encrypt
+            // 
+            button_Encrypt.CustomizableEdges = customizableEdges9;
+            button_Encrypt.DisabledState.BorderColor = Color.DarkGray;
+            button_Encrypt.DisabledState.CustomBorderColor = Color.DarkGray;
+            button_Encrypt.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            button_Encrypt.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            button_Encrypt.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            button_Encrypt.ForeColor = Color.White;
+            button_Encrypt.Location = new Point(88, 310);
+            button_Encrypt.Name = "button_Encrypt";
+            button_Encrypt.ShadowDecoration.CustomizableEdges = customizableEdges10;
+            button_Encrypt.Size = new Size(180, 45);
+            button_Encrypt.TabIndex = 0;
+            button_Encrypt.Text = "Encrypt";
+            button_Encrypt.Click += button_Encrypt_Click;
+            // 
+            // guna2HtmlLabel1
+            // 
+            guna2HtmlLabel1.BackColor = Color.Transparent;
+            guna2HtmlLabel1.Location = new Point(88, 109);
+            guna2HtmlLabel1.Name = "guna2HtmlLabel1";
+            guna2HtmlLabel1.Size = new Size(97, 17);
+            guna2HtmlLabel1.TabIndex = 1;
+            guna2HtmlLabel1.Text = "guna2HtmlLabel1";
+            // 
+            // tbWord
+            // 
+            tbWord.CustomizableEdges = customizableEdges11;
+            tbWord.DefaultText = "";
+            tbWord.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            tbWord.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            tbWord.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            tbWord.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            tbWord.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            tbWord.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            tbWord.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            tbWord.Location = new Point(88, 132);
+            tbWord.Name = "tbWord";
+            tbWord.PasswordChar = '\0';
+            tbWord.PlaceholderText = "";
+            tbWord.SelectedText = "";
+            tbWord.ShadowDecoration.CustomizableEdges = customizableEdges12;
+            tbWord.Size = new Size(200, 36);
+            tbWord.TabIndex = 2;
+            // 
+            // guna2HtmlLabel2
+            // 
+            guna2HtmlLabel2.BackColor = Color.Transparent;
+            guna2HtmlLabel2.Location = new Point(88, 194);
+            guna2HtmlLabel2.Name = "guna2HtmlLabel2";
+            guna2HtmlLabel2.Size = new Size(97, 17);
+            guna2HtmlLabel2.TabIndex = 3;
+            guna2HtmlLabel2.Text = "guna2HtmlLabel2";
+            // 
+            // tbKey
+            // 
+            tbKey.CustomizableEdges = customizableEdges13;
+            tbKey.DefaultText = "";
+            tbKey.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            tbKey.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            tbKey.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            tbKey.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            tbKey.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            tbKey.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            tbKey.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            tbKey.Location = new Point(88, 217);
+            tbKey.Name = "tbKey";
+            tbKey.PasswordChar = '\0';
+            tbKey.PlaceholderText = "";
+            tbKey.SelectedText = "";
+            tbKey.ShadowDecoration.CustomizableEdges = customizableEdges14;
+            tbKey.Size = new Size(200, 36);
+            tbKey.TabIndex = 4;
+            // 
+            // labelEncrypted
+            // 
+            labelEncrypted.BackColor = Color.Transparent;
+            labelEncrypted.Location = new Point(439, 132);
+            labelEncrypted.Name = "labelEncrypted";
+            labelEncrypted.Size = new Size(110, 17);
+            labelEncrypted.TabIndex = 5;
+            labelEncrypted.Text = "MAGIC WORD HERE";
+            // 
+            // button_Decrypt
+            // 
+            button_Decrypt.CustomizableEdges = customizableEdges15;
+            button_Decrypt.DisabledState.BorderColor = Color.DarkGray;
+            button_Decrypt.DisabledState.CustomBorderColor = Color.DarkGray;
+            button_Decrypt.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            button_Decrypt.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            button_Decrypt.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            button_Decrypt.ForeColor = Color.White;
+            button_Decrypt.Location = new Point(326, 310);
+            button_Decrypt.Name = "button_Decrypt";
+            button_Decrypt.ShadowDecoration.CustomizableEdges = customizableEdges16;
+            button_Decrypt.Size = new Size(180, 45);
+            button_Decrypt.TabIndex = 6;
+            button_Decrypt.Text = "guna2Button1";
+            // 
+            // XORForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(243, 239, 212);
+            ClientSize = new Size(716, 450);
+            Controls.Add(button_Decrypt);
+            Controls.Add(labelEncrypted);
+            Controls.Add(tbKey);
+            Controls.Add(guna2HtmlLabel2);
+            Controls.Add(tbWord);
+            Controls.Add(guna2HtmlLabel1);
+            Controls.Add(button_Encrypt);
+            Name = "XORForm";
+            Text = "XORForm";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Guna.UI2.WinForms.Guna2Button button_Encrypt;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel1;
+        private Guna.UI2.WinForms.Guna2TextBox tbWord;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel2;
+        private Guna.UI2.WinForms.Guna2TextBox tbKey;
+        private Guna.UI2.WinForms.Guna2HtmlLabel labelEncrypted;
+        private Guna.UI2.WinForms.Guna2Button button_Decrypt;
     }
 }
--- Encryption App/Forms/XORForm.cs
+++ Encryption App/Forms/XORForm.cs
@@ -16,5 +16,87 @@ namespace Encryption_App.Forms
         {
             InitializeComponent();
         }
+
+        private void button_Encrypt_Click(object sender, EventArgs e)
+        {
+            // Get the text and key from the text boxes
+            string text = tbWord.Text;
+            string key = tbKey.Text;
+
+            // Check if the key needs to be extended
+            if (key.Length < text.Length)
+            {
+                // Extend the key by repeating it
+                int repeatCount = text.Length / key.Length + 1;
+                key = key.PadRight(repeatCount * key.Length, ' ');
+            }
+
+            // Convert text and key to binary strings
+            string textBinary = StringToBinary(text);
+            string keyBinary = StringToBinary(key);
+
+            // Perform XOR operation on binary strings
+            string encryptedBinary = PerformXOR(textBinary, keyBinary);
+
+            // Display encrypted binary string
+            labelEncrypted.Text = "Encrypted Word: " + encryptedBinary;
+        }
+
+        private string StringToBinary(string input)
+        {
+            StringBuilder binary = new StringBuilder();
+            foreach (char c in input)
+            {
+                binary.Append(Convert.ToString(c, 2).PadLeft(8, '0'));
+            }
+            return binary.ToString();
+        }
+
+        private string PerformXOR(string textBinary, string keyBinary)
+        {
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < textBinary.Length; i++)
+            {
+                // Perform XOR operation on each bit
+                char xorResult = textBinary[i] == keyBinary[i] ? '0' : '1';
+                result.Append(xorResult);
+            }
+            return result.ToString();
+        }
+
+        private void button_Decrypt_Click(object sender, EventArgs e)
+        {
+            // Get the encrypted binary string
+            string encryptedBinary = labelEncrypted.Text.Substring("Encrypted Word: ".Length);
+
+            // Convert the key to binary
+            string key = tbKey.Text;
+            if (key.Length < encryptedBinary.Length / 8)
+            {
+                int repeatCount = encryptedBinary.Length / (8 * key.Length) + 1;
+                key = key.PadRight(repeatCount * key.Length, ' ');
+            }
+            string keyBinary = StringToBinary(key);
+
+            // Perform XOR operation to decrypt
+            string decryptedBinary = PerformXOR(encryptedBinary, keyBinary);
+
+            // Convert binary string to text
+            string decryptedText = BinaryToString(decryptedBinary);
+
+            // Display decrypted text
+            labelDecrypted.Text = "Decrypted Word: " + decryptedText;
+        }
+
+        private string BinaryToString(string binary)
+        {
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < binary.Length; i += 8)
+            {
+                string byteString = binary.Substring(i, 8);
+                result.Append(Convert.ToChar(Convert.ToByte(byteString, 2)));
+            }
+            return result.ToString();
+        }
     }
 }
