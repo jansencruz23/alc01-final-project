1eb2c2f3c2b1198fc40d1108371467504e3fa47f
Jansen Cruz <103548207+jansencruz23@users.noreply.github.com>
2024-05-31 19:44:49 +0800

    Change design Main form

--- Encryption App/Forms/MainMenu.Designer.cs
+++ Encryption App/Forms/MainMenu.Designer.cs
@@ -28,150 +28,139 @@
         /// </summary>
         private void InitializeComponent()
         {
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges11 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges12 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainMenu));
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges9 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges10 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges8 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
-            guna2Panel3 = new Guna.UI2.WinForms.Guna2Panel();
-            guna2Panel2 = new Guna.UI2.WinForms.Guna2Panel();
-            btnEncryption = new Guna.UI2.WinForms.Guna2Button();
-            guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
-            guna2Button2 = new Guna.UI2.WinForms.Guna2Button();
-            guna2Panel1.SuspendLayout();
-            guna2Panel2.SuspendLayout();
-            SuspendLayout();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges8 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.guna2PictureBox1 = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.guna2PictureBox2 = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            this.btnEncrypt = new Guna.UI2.WinForms.Guna2Button();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox2)).BeginInit();
+            this.SuspendLayout();
             // 
-            // guna2Panel1
+            // label1
             // 
-            guna2Panel1.BackColor = Color.Transparent;
-            guna2Panel1.BorderRadius = 25;
-            guna2Panel1.Controls.Add(guna2Panel3);
-            guna2Panel1.Controls.Add(guna2Panel2);
-            guna2Panel1.CustomizableEdges = customizableEdges11;
-            guna2Panel1.FillColor = Color.FromArgb(74, 58, 146);
-            guna2Panel1.Location = new Point(169, 36);
-            guna2Panel1.Name = "guna2Panel1";
-            guna2Panel1.ShadowDecoration.BorderRadius = 25;
-            guna2Panel1.ShadowDecoration.CustomizableEdges = customizableEdges12;
-            guna2Panel1.ShadowDecoration.Depth = 10;
-            guna2Panel1.ShadowDecoration.Enabled = true;
-            guna2Panel1.ShadowDecoration.Shadow = new Padding(3, 3, 6, 6);
-            guna2Panel1.Size = new Size(459, 306);
-            guna2Panel1.TabIndex = 0;
+            this.label1.Font = new System.Drawing.Font("Playground", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(102)))), ((int)(((byte)(3)))));
+            this.label1.Location = new System.Drawing.Point(-1, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(663, 76);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CRYPTYY";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
-            // guna2Panel3
+            // label2
             // 
-            guna2Panel3.CustomizableEdges = customizableEdges1;
-            guna2Panel3.Location = new Point(-33, 11);
-            guna2Panel3.Name = "guna2Panel3";
-            guna2Panel3.ShadowDecoration.CustomizableEdges = customizableEdges2;
-            guna2Panel3.Size = new Size(506, 64);
-            guna2Panel3.TabIndex = 0;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(-1, 159);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(663, 58);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Try it and turn your messages \r\ninto fun puzzles";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
-            // guna2Panel2
+            // guna2PictureBox1
             // 
-            guna2Panel2.BorderRadius = 25;
-            guna2Panel2.Controls.Add(guna2Button2);
-            guna2Panel2.Controls.Add(guna2Button1);
-            guna2Panel2.Controls.Add(btnEncryption);
-            guna2Panel2.CustomizableEdges = customizableEdges9;
-            guna2Panel2.FillColor = Color.FromArgb(111, 88, 202);
-            guna2Panel2.Location = new Point(3, 53);
-            guna2Panel2.Name = "guna2Panel2";
-            guna2Panel2.ShadowDecoration.CustomizableEdges = customizableEdges10;
-            guna2Panel2.Size = new Size(453, 250);
-            guna2Panel2.TabIndex = 0;
+            this.guna2PictureBox1.CustomizableEdges = customizableEdges1;
+            this.guna2PictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("guna2PictureBox1.Image")));
+            this.guna2PictureBox1.ImageRotate = 0F;
+            this.guna2PictureBox1.Location = new System.Drawing.Point(391, 285);
+            this.guna2PictureBox1.Name = "guna2PictureBox1";
+            this.guna2PictureBox1.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            this.guna2PictureBox1.Size = new System.Drawing.Size(201, 245);
+            this.guna2PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.guna2PictureBox1.TabIndex = 4;
+            this.guna2PictureBox1.TabStop = false;
             // 
-            // btnEncryption
+            // guna2PictureBox2
             // 
-            btnEncryption.BackColor = Color.FromArgb(111, 88, 202);
-            btnEncryption.BorderColor = Color.Silver;
-            btnEncryption.BorderRadius = 7;
-            btnEncryption.CustomizableEdges = customizableEdges7;
-            btnEncryption.DisabledState.BorderColor = Color.DarkGray;
-            btnEncryption.DisabledState.CustomBorderColor = Color.DarkGray;
-            btnEncryption.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
-            btnEncryption.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
-            btnEncryption.FillColor = Color.White;
-            btnEncryption.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
-            btnEncryption.ForeColor = Color.Black;
-            btnEncryption.Location = new Point(22, 41);
-            btnEncryption.Name = "btnEncryption";
-            btnEncryption.ShadowDecoration.CustomizableEdges = customizableEdges8;
-            btnEncryption.Size = new Size(406, 57);
-            btnEncryption.TabIndex = 1;
-            btnEncryption.Text = "guna2Button1";
+            this.guna2PictureBox2.CustomizableEdges = customizableEdges3;
+            this.guna2PictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("guna2PictureBox2.Image")));
+            this.guna2PictureBox2.ImageRotate = 0F;
+            this.guna2PictureBox2.Location = new System.Drawing.Point(12, 240);
+            this.guna2PictureBox2.Name = "guna2PictureBox2";
+            this.guna2PictureBox2.ShadowDecoration.CustomizableEdges = customizableEdges4;
+            this.guna2PictureBox2.Size = new System.Drawing.Size(281, 336);
+            this.guna2PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.guna2PictureBox2.TabIndex = 5;
+            this.guna2PictureBox2.TabStop = false;
             // 
             // guna2Button1
             // 
-            guna2Button1.BackColor = Color.FromArgb(111, 88, 202);
-            guna2Button1.BorderColor = Color.Silver;
-            guna2Button1.BorderRadius = 7;
-            guna2Button1.CustomizableEdges = customizableEdges5;
-            guna2Button1.DisabledState.BorderColor = Color.DarkGray;
-            guna2Button1.DisabledState.CustomBorderColor = Color.DarkGray;
-            guna2Button1.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
-            guna2Button1.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
-            guna2Button1.FillColor = Color.White;
-            guna2Button1.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
-            guna2Button1.ForeColor = Color.Black;
-            guna2Button1.Location = new Point(22, 107);
-            guna2Button1.Name = "guna2Button1";
-            guna2Button1.ShadowDecoration.CustomizableEdges = customizableEdges6;
-            guna2Button1.Size = new Size(406, 57);
-            guna2Button1.TabIndex = 2;
-            guna2Button1.Text = "guna2Button1";
+            this.guna2Button1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(157)))), ((int)(((byte)(74)))), ((int)(((byte)(67)))));
+            this.guna2Button1.BorderRadius = 10;
+            this.guna2Button1.BorderThickness = 2;
+            this.guna2Button1.CustomizableEdges = customizableEdges5;
+            this.guna2Button1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(102)))), ((int)(((byte)(3)))));
+            this.guna2Button1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.guna2Button1.ForeColor = System.Drawing.Color.White;
+            this.guna2Button1.Location = new System.Drawing.Point(230, 295);
+            this.guna2Button1.Name = "guna2Button1";
+            this.guna2Button1.ShadowDecoration.CustomizableEdges = customizableEdges6;
+            this.guna2Button1.Size = new System.Drawing.Size(203, 49);
+            this.guna2Button1.TabIndex = 7;
+            this.guna2Button1.Text = "Decrypt Code";
             // 
-            // guna2Button2
+            // btnEncrypt
             // 
-            guna2Button2.BackColor = Color.FromArgb(111, 88, 202);
-            guna2Button2.BorderColor = Color.Silver;
-            guna2Button2.BorderRadius = 7;
-            guna2Button2.CustomizableEdges = customizableEdges3;
-            guna2Button2.DisabledState.BorderColor = Color.DarkGray;
-            guna2Button2.DisabledState.CustomBorderColor = Color.DarkGray;
-            guna2Button2.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
-            guna2Button2.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
-            guna2Button2.FillColor = Color.White;
-            guna2Button2.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
-            guna2Button2.ForeColor = Color.Black;
-            guna2Button2.Location = new Point(22, 173);
-            guna2Button2.Name = "guna2Button2";
-            guna2Button2.ShadowDecoration.CustomizableEdges = customizableEdges4;
-            guna2Button2.Size = new Size(406, 57);
-            guna2Button2.TabIndex = 3;
-            guna2Button2.Text = "guna2Button2";
+            this.btnEncrypt.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(157)))), ((int)(((byte)(74)))), ((int)(((byte)(67)))));
+            this.btnEncrypt.BorderRadius = 10;
+            this.btnEncrypt.BorderThickness = 2;
+            this.btnEncrypt.CustomizableEdges = customizableEdges7;
+            this.btnEncrypt.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnEncrypt.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnEncrypt.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnEncrypt.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnEncrypt.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(102)))), ((int)(((byte)(3)))));
+            this.btnEncrypt.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnEncrypt.ForeColor = System.Drawing.Color.White;
+            this.btnEncrypt.Location = new System.Drawing.Point(230, 240);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.ShadowDecoration.CustomizableEdges = customizableEdges8;
+            this.btnEncrypt.Size = new System.Drawing.Size(203, 49);
+            this.btnEncrypt.TabIndex = 8;
+            this.btnEncrypt.Text = "Encrypt Text";
             // 
             // MainMenu
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.FromArgb(118, 95, 209);
-            ClientSize = new Size(800, 378);
-            Controls.Add(guna2Panel1);
-            Name = "MainMenu";
-            Text = "MainMenu";
-            guna2Panel1.ResumeLayout(false);
-            guna2Panel2.ResumeLayout(false);
-            ResumeLayout(false);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(239)))), ((int)(((byte)(212)))));
+            this.ClientSize = new System.Drawing.Size(661, 494);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.guna2Button1);
+            this.Controls.Add(this.guna2PictureBox2);
+            this.Controls.Add(this.guna2PictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "MainMenu";
+            this.Text = "MainMenu";
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
 
-        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
-        private Guna.UI2.WinForms.Guna2Panel guna2Panel2;
-        private Guna.UI2.WinForms.Guna2Panel guna2Panel3;
-        private Guna.UI2.WinForms.Guna2Button btnEncryption;
-        private Guna.UI2.WinForms.Guna2Button guna2Button2;
+        private Label label1;
+        private Label label2;
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox1;
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox2;
         private Guna.UI2.WinForms.Guna2Button guna2Button1;
+        private Guna.UI2.WinForms.Guna2Button btnEncrypt;
     }
 }
